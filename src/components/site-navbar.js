211028c562b33c8403665656a90a89e8255928e8
Vish Epa <vishepa@myyahoo.com>
2026-03-02 07:42:19 -0800

    created navbar as a component

---
+++ src/components/site-navbar.js
@@ -0,0 +1,73 @@
+// import { onAuthChanged } from "firebase/auth";
+// import { auth } from '/src/firebaseConfig.js';
+// import { logoutUser } from '/src/authentication.js';
+
+class SiteNavbar extends HTMLElement {
+
+    constructor() {
+        super();
+        this.renderNavbar();
+        this.renderAuthControls();
+    }
+
+    renderNavbar() {
+        this.innerHTML = `
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-info">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Find your way</a>
+                <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <p>&#9786;</p>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    />
+                    <button class="btn btn-outline-success" type="submit">
+                    Search
+                    </button>
+                </form>
+                </div>
+            </div>
+            </nav>
+        `;
+    }
+
+    // renderAuthControls(){
+    //     const authControls = this.querySelector('#authControls');
+
+    //     authControls.innerHTML = `<div class="btn btn-outline-light" style="visibility: hidden; min-width: 80px;">Log out</div>`;
+
+    //     onAuthStateChanged(auth, (user) => {
+    //         let updatedAuthControl;
+    //         if (user) {
+    //             updatedAuthControl = `<button class="btn btn-outline-light" id="signOutBtn" type="button" style="min-width: 80px;">Log out</button>`;
+    //             authControls.innerHTML = updatedAuthControl;
+    //             const signOutBtn = authControls.querySelector('#signOutBtn');
+    //             signOutBtn?.addEventListener('click', logoutUser);
+    //         } else {
+    //             updatedAuthControl = `<a class="btn btn-outline-light" id="loginBtn" href="/login.html" style="min-width: 80px;">Log in</a>`;
+    //             authControls.innerHTML = updatedAuthControl;
+    //         }
+    //     });
+    // }
+}
+
+customElements.define('site-navbar', SiteNavbar)
---
+++ src/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Configuration values are loaded securely from Vite
+// environment variables
+
+// Import Firebase SDK modules
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Read Firebase Config from vite environment 
+// variables in the .env file.
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECTS_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+
+export const auth = getAuth(app);
