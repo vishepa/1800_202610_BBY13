379936c79c8f2fa199e498adb804992e1e26762a
Vish Epa <vishepa@myyahoo.com>
2026-03-05 17:13:23 -0800

    Goated database update tools

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,11 @@
 // Configuration values are loaded securely from Vite
 // environment variables
 
+
 // Import Firebase SDK modules
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Read Firebase Config from vite environment 
 // variables in the .env file.
@@ -19,4 +21,6 @@ const app = initializeApp(firebaseConfig);
 
 
 
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
--- src/main.js
+++ src/main.js
@@ -1,14 +1,54 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import { collection, getDocs, addDoc, serverTimestamp } from 'firebase/firestore';
+
+import { db } from './firebaseConfig.js';
+import { doc, onSnapshot } from "firebase/firestore";
 
 // If you have custom global styles, import them as well:
 import '../styles/style.css';
 
 import '../src/location-tiles.js';
 
+import {
+    onAuthReady,
+} from './authentication.js';
+import { add } from 'firebase/firestore/pipelines';
+
 function sayHello() {
 
 }
+
+function addLocationData() {
+    const locationRef = collection(db, 'locations');
+
+    addDoc(locationRef, {
+        code: "TIMS", name: "Tim Hortons", congestion: "Busy", wait_time: "10-15 mins", distance: "150m away", last_updated: serverTimestamp()
+    });
+    addDoc(locationRef, {
+        code: "STARBUCKS", name: "Starbucks", congestion: "Moderate", wait_time: "5-10 mins", distance: "200m away", last_updated: serverTimestamp()
+    });
+    addDoc(locationRef, {
+        code: "TRIPLEO", name: "Triple O's", congestion: "Light", wait_time: "0-5 mins", distance: "300m away", last_updated: serverTimestamp()
+    });
+}
+
+async function seedLocations() {
+    const locationRef = collection(db, 'locations');
+
+    // retrieve all documents in the 'locations' collection
+    const querySnapshot = await getDocs(locationRef);
+
+    if (querySnapshot.empty) {
+        console.log('No location data found, seeding initial data...');
+
+        addLocationData();
+    } else {
+        console.log('Location data already exists, skipping seeding.');
+    }
+}
+
+seedLocations();
 
 
 function showName() {
